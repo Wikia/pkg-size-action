de4a435793fac13070ab3a9b401a94e67c34acd9
Jakub Bieńkowski <jbienkowski@fandom.com>
2025-01-24 13:05:22 +0100

    Emoji direction arrows

--- src/report-templates/regression/index.js
+++ src/report-templates/regression/index.js
@@ -14,11 +14,11 @@ import comparePackages from './compare-packages.js';
 
 const directionSymbol = (value) => {
 	if (value < 0) {
-		return ' :arrow_down:';
+		return ' :arrow_down: ';
 	}
 
 	if (value > 0) {
-		return ' :arrow_up:';
+		return ' :arrow_up: ';
 	}
 
 	return '';
